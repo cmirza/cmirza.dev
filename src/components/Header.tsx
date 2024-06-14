25e198439e9d08af8268f2e64ba9749510fbbde6
cmirza <mail@cmirza.com>
2024-06-14 15:07:06 -0700

    fix avatar shape

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,13 @@ const Header: React.FC = () => {
     <header className="p-4">
       <div className="max-w-xl mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
-          <div className="w-14 h-14 rounded-full overflow-hidden">
+          <div className="relative w-14 h-14 rounded-full overflow-hidden">
             <Image
               src="https://github.com/cmirza.png"
               alt="avatar"
-              width={40}
-              height={40}
-              className="object-cover"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
             />
           </div>
           <div>
